682da81fb7c8a4b094e3e731c84ae9855396a129
NaN Gogh <igor@torbas.net>
2026-01-09 19:09:24 +0100

    chore: commit all pending changes

---
+++ data/layers/geCrystalStorage.js
@@ -0,0 +1,20 @@
+// GE Crystal Storage layer
+// Requires data/init.js to be loaded first
+LAYERS.geCrystalStorage = {
+    name: "GE Crystal Storage",
+    icon: "🪣",
+    color: "#1d6722ff",
+    prefix: "gs",
+    markers: [
+        {"uid": "gs_12db0f78", "x": 0.41971399688473565, "y": 0.2762936508837572},
+        {"uid": "gs_6e96e75e", "x": 0.19601550597465872, "y": 0.35267726457607396},
+        {"uid": "gs_7abec50d", "x": 0.6862480485406756, "y": 0.425621376884822},
+        {"uid": "gs_7b248b77", "x": 0.5863995174185251, "y": 0.7017653312410784},
+        {"uid": "gs_57966d4c", "x": 0.8593964200262402, "y": 0.7603366099252687},
+        {"uid": "gs_6095af14", "x": 0.41434539677855015, "y": 0.7548209382014334}
+    ]
+};
+
+// Convenience export for backward compatibility
+const GE_CRYSTAL_STORAGE = LAYERS.geCrystalStorage.markers;
+
